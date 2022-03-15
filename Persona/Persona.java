3ef54d0d285ff746d74d57ad48b699375931a262
Nidrio <ivan.nidriotakis@itponiente.com>
2022-03-15 13:18:20 +0100

    Explicacion código

--- Persona/Persona.java
+++ Persona/Persona.java
@@ -1,1 +1,3 @@
-Public void Persona
+// Clase persona herencia
+// Dos tipos de herencia: Extension(a los metodos del padre le añadimo metodos propios) y especializacion(modificamos los métodos del padre para adaptarlos a nuestras necesidades).
+//Además de la herencia por ahregacion o composicion (añadir clases como atributos de nuestra clase) explicado en coche.
