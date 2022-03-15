449c48c6281ffbbcf6ccc740b2558fd08b96c987
Nidrio <ivan.nidriotakis@itponiente.com>
2022-03-15 13:15:07 +0100

    Coche terminado

---
+++ Carro/Main.java
@@ -0,0 +1,27 @@
+class Main {
+	public static void main(String[] args) {
+
+		Motor miMotor = new Motor();
+		Ventana miVentana = new Ventana();
+		Puerta miPuerta = new Puerta();
+    Coche miCoche = new Coche();
+    System.out.println(miCoche);
+    //Ver como esta el motor:
+    System.out.println(miCoche.getMotor());
+
+		System.out.println(miPuerta);
+    //Ver como esta la ventana de la puerta derecha:
+		System.out.println(miCoche.getPuertaDer());
+		//Abrir puerta derecha:
+		miCoche.getPuertaDer().abrir();
+		System.out.println(miCoche);
+		//Ver como esta la ventana de la puerta derecha:
+		System.out.println(miCoche.getPuertaDer().getVentana());
+
+
+
+
+
+
+	}
+}
