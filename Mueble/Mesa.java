d5e1444b6344d8cbdd4f8635940cbd7ca8c08d50
Nidrio <ivan.nidriotakis@itponiente.com>
2022-03-31 12:29:39 +0200

    mesa

---
+++ Mueble/Mesa.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Mesa extends Mueble{
+    private int nPatas;
+
+
+public Mesa(){
+  super();
+  this.nPatas = 4;
+  }
+
+public Mesa(String material, ArrayList<Integer> dimensiones, int nPatas){
+  super (material, dimensiones);
+  this.nPatas = nPatas;
+}
+
+
+//Get
+public int getNPatas(){
+  return this.nPatas;
+  }
+
+//SET
+public void setNPatas(int nPatas){
+  this.nPatas = nPatas;
+    }
+
+//TO STRING
+
+public String toString(){
+  return super.toString() + "La mesa tiene " + this.nPatas + " patas.";
+  }
+
+}
