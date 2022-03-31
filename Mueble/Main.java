80dacafce0768adc71434de331a7530edf003b83
Nidrio <ivan.nidriotakis@itponiente.com>
2022-03-31 12:10:06 +0200

    Main

---
+++ Mueble/Main.java
@@ -0,0 +1,24 @@
+class Main{
+  public static void main(String[] args){
+    Mueble mesa = new Mueble();
+    System.out.println(mesa);
+    mesa.setMaterial("Madera");
+    mesa.setDimensiones(12, 44, 27);
+    System.out.println(mesa);
+    System.out.println(mesa.Superficie(50,20));
+
+
+    Mesa miMesa = new Mesa();
+    System.out.println(miMesa);
+    miMesa.setNPatas(5);
+    miMesa.setDimensiones(12,7,18);
+    System.out.println(miMesa);
+    System.out.println(miMesa.Superficie(12, 7));
+
+    Armario miArmario = new Armario();
+    System.out.println(miArmario);
+    miArmario.setTipoPuerta("Puerta doble");
+    miArmario.setNBaldas(3);
+    System.out.println(miArmario);
+    }
+}
