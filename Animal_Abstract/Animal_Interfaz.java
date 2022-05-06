257bd9e7e1ad44b8f38eff4e0d8447af4837d990
Nidrio <ivan.nidriotakis@itponiente.com>
2022-05-06 12:26:14 +0200

    animal miaur

---
+++ Animal_Abstract/Animal_Interfaz.java
@@ -0,0 +1,9 @@
+public interface Animal_Intrfz{
+  // Al declarar variables en una interfaz hay que inicializarlas.
+  // Pero no sirve para nada porque no se heredan xd.
+  public String alimentacion = "comida";
+
+  public String durmiendo();
+  public String comiendo();
+  public String onomatopeya();
+}
--- Animal_Abstract/Gato.java
+++ Animal_Abstract/Gato.java
@@ -3,10 +3,23 @@ class Gato extends Animal{
     return "Miau";
   }
   public String durmiendo(){
-    return super.durmiendo() + " -> RRrrrrRRRrrr";
+    return super.durmiendo() + " -> RRRrrrrRRRRrrRRRrrR";
   }
   public String comiendo(){
-    this.alimentacion = "Pescao";
-     return "Yo como" + this.alimentacion;
+    this.alimentacion = "Peces";
+    return "Yo como " + this.alimentacion;
+  }
+}
+
+class Gato implements Animal_Intrfz{
+  public String onomatopeya(){
+    return "Miau";
+  }
+  public String durmiendo(){
+    return "ZzzZz -> RRRrrrrRRRRrrRRRrrR";
+  }
+  public String comiendo(){
+    String alimentacion = "peces";
+    return "Yo como " + alimentacion;
   }
 }
